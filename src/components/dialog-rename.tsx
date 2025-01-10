d544c3215c0827f26c7fa8cf09350c9accab9e8e
Thi <thi@ideta.io>
2025-01-10 04:28:31 +0100

    style: dialog styles

--- src/components/dialog-rename.tsx
+++ src/components/dialog-rename.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import * as VisuallyHidden from '@radix-ui/react-visually-hidden'
 import { useState } from 'react'
 import { Button } from './ui/button'
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from './ui/dialog'
@@ -26,12 +27,17 @@ export function RenameDialog({ open, onOpenChange, title, onRename }: RenameDial
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent hideCloseBtn={true} className='!rounded-3xl p-6 gap-6 flex flex-col'>
-        <DialogHeader className='gap-2'>
-          <DialogTitle>Rename Chat</DialogTitle>
-          <DialogDescription>Current title: <span className='font-medium'>{title}</span></DialogDescription>
+      <DialogContent hideCloseBtn={true} className="flex flex-col gap-4 px-6 py-4">
+        <DialogHeader className="gap-2">
+          <VisuallyHidden.Root>
+            <DialogTitle>Rename Chat</DialogTitle>
+          </VisuallyHidden.Root>
+          <DialogDescription className="text-foreground">
+            Current title: <span className="font-medium">{title}</span>
+          </DialogDescription>
         </DialogHeader>
         <Input
+          className="mb-2 rounded-none border-x-0 border-t-0 px-2 pb-0 !text-base"
           value={newTitle}
           onChange={e => setNewTitle(e.target.value)}
           placeholder="Enter new title"
