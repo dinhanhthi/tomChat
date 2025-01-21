132e999b3e2f38ba59582f62afb4ed9e39769dc0
Thi <dinhanhthi@gmail.com>
2025-01-21 20:37:21 +0100

    style: fixed border radius for popover content

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,7 @@ export const BRAND_NAME = 'TomChat'
 export const BRAND_DESCRIPTION = 'An UI to run seamlessly LLM services (OpenAI, Claude, Gemini, Mistral,...)'
 export const MODAL_BG_CLASS = 'bg-black/30'
 export const MODAL_RADIUS = '!rounded-2xl'
+export const POPOVER_RADIUS = '!rounded-xl'
 export const N_WELCOME_SEARCH_RESULTS = 7
 export const NO_TAG = 'No tag' // user can't create tag with this name
 export const MAX_N_TAGS = 20
